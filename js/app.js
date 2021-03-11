952cb0717de9034de223b9a8a4105d91eb6afde9
fahraday <bravokevinto@gmail.com>
2021-03-11 13:59:30 -0400

    Arreglo de estructura de codigo de archivo js
    se arreglo el codigo js para colocar tambien en pantallas tactiles
    Se realizo el arreglo del bug que no quitaba el estado de css

--- js/app.js
+++ js/app.js
@@ -1,17 +1,45 @@
+const KEY = {
+  'A': 65,
+  'S': 83,
+  'D': 68,
+  'F': 70,
+  'G': 71,
+  'H': 72,
+  'J': 74,
+  'K': 75,
+  'L': 76,
+};
+
+
+
+function removeTransition(e) {
+  // if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function playSoundOnTouch(e) {
+  const audio = document.querySelector(`audio[data-key="${KEY[e.path[0].innerText]}"]`);
+  const key = document.querySelector(`div[data-key="${KEY[e.path[0].innerText]}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
 const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
-window.addEventListener('keydown', playSound)
-function playSound (e){
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-	const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
-	if (!audio) return;
-	key.classList.add('playing')
-	audio.currentTime = 0;
-	audio.play();
-}
-
-function removeTransition(e){
-	if(e.propertyName !== 'transform') return;
-	e.target.classList.remove('playing');
-}
-
+keys.forEach((key) => {
+  key.addEventListener('transitionend', removeTransition);
+  key.addEventListener('touchstart', playSoundOnTouch, false);
+  key.addEventListener('click', playSoundOnTouch);
+});
+window.addEventListener('keydown', playSound);
