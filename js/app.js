3f6b7f5c22790340db8a17460f08fde0f54e6171
fahraday <bravokevinto@gmail.com>
2021-03-20 13:23:22 -0400

    shit done, I don't remember what changes i did xd

--- js/app.js
+++ js/app.js
@@ -1,25 +1,43 @@
 const instruments = document.querySelectorAll(".instrument")
 const keys = Array.from(document.querySelectorAll('.key'));
 let selectedInstrument = ""
-var instrumentsText = Array.from(document.querySelectorAll(".sound"))
+const instrumentsText = Array.from(document.querySelectorAll(".sound"))
+const keysContainer = document.querySelector(".keys_container")
+const instrumentContainer = document.querySelector(".instrument_container")
+const welcomeContainer = document.querySelector(".welcome_container")
+const btnBack = document.querySelector(".backBtn")
+
+const notFound = document.querySelector(".notFound")
+
+let isInPage = false
+const subtitle = document.querySelector(".h2")
 
 var instrumentTextContent = instrumentsText.textContent
 
-var guitarTexts = ["c major", "d major", "e major", "f major", "g major", "a major", "b major"];
+var guitarTexts = ["c", "d", "e", "f", "g", "a", "b"];
 
 var drummTexts = ["boom", "clap", "hihat", "kick", "openhat", "ride", "Snare", "tink", "tom"];
 
 var funnyTexts = ["air horn", "bong", "bonk", "negative sound", "pop up", "no", "yeiih"];
 
 
-var guitarAudios = [];
-guitarAudios["65"] = new Audio("../../sounds/guitar/cleanchord-Cm.wav");
-guitarAudios["83"] = new Audio("../../sounds/guitar/cleanchord-D.wav");
-guitarAudios["68"] = new Audio("../../sounds/guitar/cleanchord-E.wav");
-guitarAudios["70"] = new Audio("../../sounds/guitar/cleanchord-F.wav");
-guitarAudios["71"] = new Audio("../../sounds/guitar/cleanchord-G.wav");
-guitarAudios["72"] = new Audio("../../sounds/guitar/cleanchord-A.wav");
-guitarAudios["74"] = new Audio("../../sounds/guitar/cleanchord-B.wav");
+var guitarAudiosMajor = [];
+guitarAudiosMajor["65"] = new Audio("../../sounds/guitar/cleanchord-Cm.wav");
+guitarAudiosMajor["83"] = new Audio("../../sounds/guitar/cleanchord-D.wav");
+guitarAudiosMajor["68"] = new Audio("../../sounds/guitar/cleanchord-E.wav");
+guitarAudiosMajor["70"] = new Audio("../../sounds/guitar/cleanchord-F.wav");
+guitarAudiosMajor["71"] = new Audio("../../sounds/guitar/cleanchord-G.wav");
+guitarAudiosMajor["72"] = new Audio("../../sounds/guitar/cleanchord-A.wav");
+guitarAudiosMajor["74"] = new Audio("../../sounds/guitar/cleanchord-B.wav");
+
+var guitarAudiosMinor = [];
+guitarAudiosMinor["65"] = new Audio("../../sounds/guitar/cleanchord-Cm.wav");
+guitarAudiosMinor["83"] = new Audio("../../sounds/guitar/cleanchord-Dm.wav");
+guitarAudiosMinor["68"] = new Audio("../../sounds/guitar/cleanchord-Em.wav");
+guitarAudiosMinor["70"] = new Audio("../../sounds/guitar/cleanchord-Fm.wav");
+guitarAudiosMinor["71"] = new Audio("../../sounds/guitar/cleanchord-Gm.wav");
+guitarAudiosMinor["72"] = new Audio("../../sounds/guitar/cleanchord-Am.wav");
+guitarAudiosMinor["74"] = new Audio("../../sounds/guitar/cleanchord-Bm.wav");
 
 var funnyAudios = [];
 funnyAudios["65"] = new Audio("../../sounds/funny/Air-horn-noise.mp3");
@@ -69,13 +87,28 @@ function removeTransition(e) {
 
 function playSound(e) {
 
+subtitle.classList.add("h2dissapear")
+
   if(selectedInstrument == "guitar"){
-    const audio = guitarAudios[e.keyCode];
+
+    if(e.shiftKey){
+      const audio = guitarAudiosMinor[e.keyCode];
+      const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+      if (!audio) return;
+      key.classList.add('playing');
+      audio.currentTime = 0;
+      audio.play();
+
+    }
+    else{
+    const audio = guitarAudiosMajor[e.keyCode];
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
+
+  }
     }
     else if(selectedInstrument == "funny_sounds"){
       const audio = funnyAudios[e.keyCode];
@@ -96,22 +129,46 @@ function playSound(e) {
     }
 }
 
-// function playSoundOnTouch(e) {
-//   const audio = document.querySelector(`audio[data-key="${KEY[e.path[0].innerText]}"]`);
-//   const key = document.querySelector(`div[data-key="${KEY[e.path[0].innerText]}"]`);
-//     console.log(key, "sonidda", e.path)
+function playSoundOnTouch(e) {
+  if(selectedInstrument == "guitar"){
+      const audio = guitarAudiosMinor[KEY[e.path[0].innerText]];
+      const key = document.querySelector(`div[data-key="${KEY[e.path[0].innerText]}"]`);
+      if (!audio) return;
+      key.classList.add('playing');
+      audio.currentTime = 0;
+      audio.play()
+    }
+    else if(selectedInstrument == "funny_sounds"){
+      const audio = funnyAudios[KEY[e.path[0].innerText]];
+      const key = document.querySelector(`div[data-key="${KEY[e.path[0].innerText]}"]`);
+      if (!audio) return;
+      key.classList.add('playing');
+      audio.currentTime = 0;
+      audio.play();
+    }
 
-//   if (!audio) return;
-
-//   key.classList.add('playing');
-//   audio.currentTime = 0;
-//   audio.play();
-// }
+    else if(selectedInstrument == "drumm"){
+      const audio = drummAudios[KEY[e.path[0].innerText]];
+      const key = document.querySelector(`div[data-key="${KEY[e.path[0].innerText]}"]`);
+      if (!audio) return;
+      key.classList.add('playing');
+      audio.currentTime = 0;
+      audio.play();
+    }
+}
 
 
 function selectInstrument(e){
+
+  keys.forEach(key => {
+    key.style.display =""
+  })
+
+  isInPage = true
   const instrument = document.querySelector(`div[data-instrument="${INSTRUMENTS[e.path[0].innerText]}"]`);
+
   selectedInstrument = instrument.dataset.instrument
+
   if (selectedInstrument == "drumm"){
     drummTexts.forEach(e =>{
       for(i in instrumentsText){
@@ -122,6 +179,7 @@ function selectInstrument(e){
       }
     })
   }
+
   else if (selectedInstrument == "guitar") {
     guitarTexts.forEach(e =>{
       for(i in instrumentsText){
@@ -132,10 +190,7 @@ function selectInstrument(e){
       }
     })
   }
-  else if (selectedInstrument == "piano") {
-  }
-  else if (selectedInstrument == "bass") {
-  }
+
   else if (selectedInstrument == "funny_sounds") {
     funnyTexts.forEach(e =>{
       for(i in instrumentsText){
@@ -146,6 +201,37 @@ function selectInstrument(e){
       }
     })
   }
+//para borrar las teclas sobresalientes
+  let toDelete = keys.map(key =>{
+    let borralo = key.lastElementChild
+    if(borralo.textContent == ""){
+        return key
+    }
+  })
+
+  let toDeleteFilter = toDelete.filter(x => x !== undefined);
+  if(toDeleteFilter.length >= 0){
+    toDeleteFilter.forEach(del => {
+      del.style.display ="none"
+    })
+  }
+
+  btnBack.classList.add("btndDisplaying")
+  keysContainer.classList.add("keysContainerDisplay")
+  welcomeContainer.style.display = "none"
+  instrumentContainer.style.display = "none"  
+
+  notFound.classList.remove("notFoundDisplay")
+
+  if (selectedInstrument == "piano") {
+    notFound.classList.add("notFoundDisplay")
+    keysContainer.classList.remove("keysContainerDisplay")
+  }
+
+  else if (selectedInstrument == "bass") {
+  notFound.classList.add("notFoundDisplay")
+  keysContainer.classList.remove("keysContainerDisplay")
+  }
 }
 
 instruments.forEach((instrument) => {
@@ -154,7 +240,22 @@ instruments.forEach((instrument) => {
 
 keys.forEach((key) => {
   key.addEventListener('transitionend', removeTransition);
-  // key.addEventListener('touchstart', playSoundOnTouch, false);
-  // key.addEventListener('click', playSoundOnTouch);
+  key.addEventListener('touchstart', playSoundOnTouch, false);
+  key.addEventListener('click', playSoundOnTouch);
 });
 window.addEventListener('keydown', playSound);
+
+
+
+
+btnBack.addEventListener('click', back =>{
+  if(back.target.matches("button")){
+    keysContainer.classList.remove("keysContainerDisplay")
+    welcomeContainer.display = ''
+    btnBack.classList.remove("btndDisplaying")
+    instrumentContainer.style.display = ""
+    subtitle.classList.remove("h2dissapear")
+    notFound.classList.remove("notFoundDisplay")
+
+  }
+})
